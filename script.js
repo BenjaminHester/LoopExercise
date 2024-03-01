7fe7305766f9976c97cee06c80b828382234d7ce
Benjamin Hester <benjamin.hester@stu.barren.kyschools.us>
2024-03-01 09:37:27 -0600

    Finished Loop Exercise.

---
+++ script.js
@@ -0,0 +1,19 @@
+for (i=1;i<=7;i++){
+    console.log(i);
+}
+for (c=5;c<=25;c+=4){
+    console.log(c);
+}
+const wizards = [
+    `Harry Potter`,
+    `Hermione Granger`,
+    `Ron Weasley`
+]
+for (wizzys of wizards){
+    console.log(wizzys);
+}
+let harryPotterMovies = 0;
+while (harryPotterMovies<=8){
+    console.log(harryPotterMovies);
+    harryPotterMovies++
+};
